6580621694f8348ed23b9ff3779d428ab3916308
Justin Diaz <justhewebdev@gmail.com>
2024-05-25 20:11:59 -0400

    selected required elements

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,27 @@
 import "../styles.css";
+
+const balanceEl = document.querySelector(".balance .value");
+
+const outcomeTotalEl = document.querySelector(".income-total");
+const incomeTotalEl = document.querySelector(".outcome-total");
+const chartEl = document.querySelector(".chart");
+
+const expenseBtn = document.querySelector(".expense-tab");
+const incomeBtn = document.querySelector(".income-tab");
+const allBtn = document.querySelector(".all-tab");
+
+const expenseEl = document.querySelector("#expense");
+const incomeEl = document.querySelector("#income");
+const allEl = document.querySelector("#all");
+
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+const addIncome = document.querySelector(".add-income-btn");
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
+
+const addExpense = document.querySelector(".add-expense-btn");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
