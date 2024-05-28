5df1723e6ccff1bb0cdf806e2d3d8e235f92d377
Justin Diaz <justhewebdev@gmail.com>
2024-05-28 19:38:24 -0400

    add title to income list

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,13 @@
 import "../styles.css";
 
+type Entry = {
+  type: "expense" | "income";
+  title: string;
+  amount: number;
+};
+
+let ENTRY_LIST: Entry[] = [];
+
 const balanceEl = document.querySelector(".balance .value");
 
 // Account elements
@@ -8,16 +16,14 @@ const incomeTotalEl = document.querySelector(".outcome-total");
 const chartEl = document.querySelector(".chart");
 
 // Dashboard navigation tab elements
-const expenseBtn: HTMLButtonElement | null =
-  document.querySelector(".expense-tab");
-const incomeBtn: HTMLButtonElement | null =
-  document.querySelector(".income-tab");
-const allBtn: HTMLButtonElement | null = document.querySelector(".all-tab");
+const expenseBtn: HTMLElement | null = document.querySelector(".expense-tab");
+const incomeBtn: HTMLElement | null = document.querySelector(".income-tab");
+const allBtn: HTMLElement | null = document.querySelector(".all-tab");
 
 // Dashboard tab section elements
-const expenseEl = document.querySelector("#expense");
-const incomeEl = document.querySelector("#income");
-const allEl = document.querySelector("#all");
+const expenseEl: HTMLElement | null = document.querySelector("#expense");
+const incomeEl: HTMLElement | null = document.querySelector("#income");
+const allEl: HTMLElement | null = document.querySelector("#all");
 
 // Section list elements
 const incomeList = document.querySelector("#income .list");
@@ -26,8 +32,10 @@ const allList = document.querySelector("#all .list");
 
 // Income input elements
 const addIncome = document.querySelector(".add-income-btn");
-const incomeTitle = document.getElementById("income-title-input");
-const incomeAmount = document.getElementById("income-amount-input");
+const incomeTitle = document.getElementById("incomeTitle") as HTMLInputElement;
+const incomeAmount = document.getElementById(
+  "incomeAmount",
+) as HTMLInputElement;
 
 // Expense input elements
 const addExpense = document.querySelector(".add-expense-btn");
@@ -55,22 +63,52 @@ allBtn?.addEventListener("click", () => {
   hide([expenseEl, incomeEl]);
 });
 
-function active(element: HTMLButtonElement) {
-  element.classList.add("active");
+function active(element: HTMLElement | null) {
+  element?.classList.add("active");
 }
 
-function inactive(elements: any[]) {
+function inactive(elements: Array<HTMLElement | null>) {
   elements.forEach((element) => {
-    element.classList.remove("active");
+    element?.classList.remove("active");
   });
 }
 
-function show(element: any) {
-  element.classList.remove("hide");
+function show(element: HTMLElement | null) {
+  element?.classList.remove("hide");
 }
 
-function hide(elements: any[]) {
+function hide(elements: Array<HTMLElement | null>) {
   elements.forEach((element) => {
-    element.classList.add("hide");
+    element?.classList.add("hide");
+  });
+}
+
+addIncome?.addEventListener("click", () => {
+  const income: Entry = {
+    type: "income",
+    title: incomeTitle?.value,
+    amount: parseFloat(incomeAmount?.value),
+  };
+
+  console.log(income);
+  ENTRY_LIST.push(income);
+  updateUI();
+  //   clearInput([incomeTitle, incomeAmount]);
+});
+
+function updateUI() {
+  ENTRY_LIST.forEach((entry) => {
+    const listEl = document.createElement("li");
+    const titleEl = document.createElement("p");
+
+    titleEl.innerText = entry.title;
+    listEl.appendChild(titleEl);
+    incomeList?.appendChild(listEl);
+  });
+}
+
+function clearInput(inputs: HTMLInputElement[]) {
+  inputs?.forEach((input) => {
+    input.value = "";
   });
 }
