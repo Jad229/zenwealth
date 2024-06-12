e21ea172984c2f97875d737bc2c888f82b7e167c
Justin Diaz <justhewebdev@gmail.com>
2024-06-12 06:36:57 -0400

    modal for entry menu implemented

--- src/js/eventHandlers.ts
+++ src/js/eventHandlers.ts
@@ -1,53 +1,11 @@
-// eventListeners.ts
-import {
-  active,
-  addIncome,
-  allBtn,
-  allEl,
-  expenseBtn,
-  expenseEl,
-  hide,
-  inactive,
-  incomeAmount,
-  incomeBtn,
-  incomeEl,
-  incomeTitle,
-  show,
-  updateUI,
-} from "./ui";
+import { buttonEntry, containerEntryMenu, overlay } from "./ui";
 
-import { ENTRY_LIST } from "./data";
-import { Entry } from "./types";
-
-expenseBtn?.addEventListener("click", () => {
-  active(expenseBtn);
-  inactive([incomeBtn, allBtn]);
-  show(expenseEl);
-  hide([incomeEl, allEl]);
+buttonEntry.addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+  containerEntryMenu.classList.remove("hidden");
 });
 
-incomeBtn?.addEventListener("click", () => {
-  active(incomeBtn);
-  inactive([expenseBtn, allBtn]);
-  show(incomeEl);
-  hide([expenseEl, allEl]);
-});
-
-allBtn?.addEventListener("click", () => {
-  active(allBtn);
-  inactive([expenseBtn, incomeBtn]);
-  show(allEl);
-  hide([expenseEl, incomeEl]);
-});
-
-addIncome?.addEventListener("click", () => {
-  const income: Entry = {
-    type: "income",
-    title: incomeTitle?.value || "",
-    amount: parseFloat(incomeAmount?.value) || 0,
-  };
-
-  ENTRY_LIST.push(income);
-  updateUI();
-  // clearInput([incomeTitle, incomeAmount]);
+overlay.addEventListener("click", () => {
+  containerEntryMenu.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
--- src/js/ui.ts
+++ src/js/ui.ts
@@ -1,106 +1,28 @@
 import { ENTRY_LIST } from "./data";
 import { Entry } from "./types";
 
-const balanceEl = document.querySelector<HTMLElement>(".balance .value");
+/* Elements */
+export const overlay = select<HTMLDivElement>("#overlay");
 
 // Account elements
-const outcomeTotalEl =
-  document.querySelector<HTMLParagraphElement>(".income-total");
-const incomeTotalEl =
-  document.querySelector<HTMLParagraphElement>(".outcome-total");
-const chartEl = document.querySelector<HTMLDivElement>(".chart");
+const balanceEl = select<HTMLElement>(".balance .value");
+const outcomeTotalEl = select<HTMLParagraphElement>(".income-total");
+const incomeTotalEl = select<HTMLParagraphElement>(".outcome-total");
+const chartEl = select<HTMLDivElement>(".chart");
 
-// Dashboard navigation tab elements
-export const expenseBtn =
-  document.querySelector<HTMLButtonElement>(".expense-tab");
-export const incomeBtn =
-  document.querySelector<HTMLButtonElement>(".income-tab");
-export const allBtn = document.querySelector<HTMLButtonElement>(".all-tab");
+//Entry Selection Elements
+export const containerEntryMenu = select<HTMLDivElement>("#entryMenu");
 
-// Dashboard tab section elements
-export const expenseEl = document.querySelector<HTMLElement>("#expense");
-export const incomeEl = document.querySelector<HTMLElement>("#income");
-export const allEl = document.querySelector<HTMLElement>("#all");
+// Navigation Elements
+export const buttonEntry = select<HTMLButtonElement>(".add-entry-button");
 
-// Section list elements
-const incomeList = document.querySelector<HTMLUListElement>("#income .list");
-const expenseList = document.querySelector<HTMLUListElement>("#expense .list");
-const allList = document.querySelector<HTMLUListElement>("#all .list");
+//Budget Elements
+const container = select<HTMLElement>(".budget-dashboard");
 
-// Income input elements
-export const addIncome =
-  document.querySelector<HTMLButtonElement>(".add-income-btn");
-export const incomeTitle = document.getElementById(
-  "incomeTitle",
-) as HTMLInputElement;
-export const incomeAmount = document.getElementById(
-  "incomeAmount",
-) as HTMLInputElement;
-
-// Expense input elements
-const addExpense =
-  document.querySelector<HTMLButtonElement>(".add-expense-btn");
-const expenseTitle = document.getElementById(
-  "expense-title-input",
-) as HTMLInputElement;
-const expenseAmount = document.getElementById(
-  "expense-amount-input",
-) as HTMLInputElement;
-
-export const active = function (element: HTMLElement | null) {
-  element?.classList.add("active");
-};
-
-export const inactive = function (elements: Array<HTMLElement | null>) {
-  elements.forEach((element) => {
-    element?.classList.remove("active");
-  });
-};
-
-export const show = function (element: HTMLElement | null) {
-  element?.classList.remove("hide");
-};
-
-export const hide = function (elements: Array<HTMLElement | null>) {
-  elements.forEach((element) => {
-    element?.classList.add("hide");
-  });
-};
-
-export const updateUI = function () {
-  // guard against null element
-  if (!incomeList) return;
-
-  // empty the incomeList element
-  incomeList.innerHTML = "";
-
-  // loop through the entries and create a list item
-  ENTRY_LIST.forEach((entry) => {
-    const listEl = createListElement(entry);
-    incomeList?.appendChild(listEl);
-  });
-};
-
-function createListElement(entry: Entry): HTMLElement {
-  const listEl = createElementWithClass("li", "income");
-  const colorEl = createElementWithClass("div", "list-item-color");
-  const titleEl = createElementWithClass("h3", "list-item-title");
-  const amountEl = createElementWithClass("p", "list-item-amount");
-  const buttonsEl = createElementWithClass("div", "list-item-buttons");
-  const editEl = createElementWithClass("button", "list-item-edit");
-  const deleteEl = createElementWithClass("button", "list-item-delete");
-
-  titleEl.innerText = entry.title;
-  amountEl.innerText = `$${entry.amount.toString()}`;
-
-  buttonsEl.appendChild(editEl);
-  buttonsEl.appendChild(deleteEl);
-  listEl.appendChild(colorEl);
-  listEl.appendChild(titleEl);
-  listEl.appendChild(amountEl);
-  listEl.appendChild(buttonsEl);
-
-  return listEl;
+// Custom function to select elements for readability
+function select<Type>(query: string): Type {
+  const htmlElement = <Type>document.querySelector(query);
+  return htmlElement;
 }
 
 function createElementWithClass(
